121dd5013044aa40bb98ce2c0a99b9f56c8f5b4d
Martin Pham <martin.liem.pham@gmail.com>
2018-05-08 19:03:45 -0400

    added firebase and started facebook auth

--- main.js
+++ main.js
@@ -1,52 +1,66 @@
-$document.ready(function() {
-  $.validator.addMethod(
-    'phoneUS',
-    function(phone_number, element) {
-      phone_number = phone_number.replace(/\s+/g, '');
-      return (
-        this.optional(element) ||
-        (phone_number.length > 9 &&
-          phone_number.match(/^\(?(\d{3})\)?[-\. ]?(\d{3})[-\. ]?(\d{4})$/))
-      );
-    },
-    'Please enter a 10 digit number'
-  );
+import * as firebase from 'firebase';
+// // Firebase App is always required and must be first
+// var firebase = require('firebase/app');
+// var firebase = require('firebase');
+//
+// // Add additional services you want to use
+// require('firebase/auth');
+// require('firebase/database');
+// require('firebase/firestore');
+// require('firebase/messaging');
+// require('firebase/functions');
+//
+// // var firebase = require('firebase');
+// var firebaseui = require('firebaseui');
+//
+// // Comment out (or don't require) services you don't want to use
+// // require("firebase/storage");
+//
+// var config = {
+//   // ...
+// };
+// firebase.initializeApp(config);
 
-  // radio button for choosing locationList
+// jQuery validation code
 
-  // $('.list-unstyled > li').click(function() {
-  //   $('.list-unstyled > li')
-  //     .find('input[type="radio"]')
-  //     .removeAttr('checked');
-  //   $(this)
-  //     .find('input[type="radio"]')
-  //     .attr('checked', 'checked');
-  // });
-
-  $(function() {
-    $("form[name='climberInfo']").validate({
-      rules: {
-        firstName: 'required',
-        lastName: 'required',
-        age: {
-          required: true
-          // ageSelector: true,
-          // min: 15,
-          // max: 60
-        },
-        // leadingGrade: 'required',
-        // boulderingGrade: 'required',
-        phoneNumber: {
-          required: true,
-          phoneUS: true
-        },
-        // days: 'required',
-        // time: 'required',
-        email: {
-          required: true,
-          email: true
-        }
-      }
-    });
-  });
-});
+// $document.ready(function() {
+//   $.validator.addMethod(
+//     'phoneUS',
+//     function(phone_number, element) {
+//       phone_number = phone_number.replace(/\s+/g, '');
+//       return (
+//         this.optional(element) ||
+//         (phone_number.length > 9 &&
+//           phone_number.match(/^\(?(\d{3})\)?[-\. ]?(\d{3})[-\. ]?(\d{4})$/))
+//       );
+//     },
+//     'Please enter a 10 digit number'
+//   );
+//
+//   $(function() {
+//     $("form[name='climberInfo']").validate({
+//       rules: {
+//         firstName: 'required',
+//         lastName: 'required',
+//         age: {
+//           required: true
+//           // ageSelector: true,
+//           // min: 15,
+//           // max: 60
+//         },
+//         // leadingGrade: 'required',
+//         // boulderingGrade: 'required',
+//         phoneNumber: {
+//           required: true,
+//           phoneUS: true
+//         },
+//         // days: 'required',
+//         // time: 'required',
+//         email: {
+//           required: true,
+//           email: true
+//         }
+//       }
+//     });
+//   });
+// });
